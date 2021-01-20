ff72334ca14da1eb5432271acc88ad0b6dc15e6b
austintorres <torresgaustin@gmail.com>
2021-01-20 11:26:02 -0500

    start Row component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>test</h1>
+      <Row title="Netflix Originals" />
     </div>
   );
 }
