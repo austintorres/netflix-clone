4f51a5f33d70440a000851b5a8658efd9484e48e
austintorres <torresgaustin@gmail.com>
2021-04-24 12:21:41 -0400

    start build for rows and axios calls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
-      <Row title="Netflix Originals" />
+      <h1>Netflix</h1>
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
